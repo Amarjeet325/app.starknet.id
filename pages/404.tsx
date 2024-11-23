dcb2c7b4212d40d5d0bb445a340fc5f588b4dcbf
Amarjeet patel <130923139+Amarjeet325@users.noreply.github.com>
2024-11-23 10:15:18 +0530

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,8 +3,11 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
-const NotFound: NextPage = () => {
++import { useRouter } from 'next/router';
++
+ const NotFound: NextPage = () => {
++  const router = useRouter();
++
   return (
     <div className={styles.screen}>
       <Head>
@@ -24,7 +27,8 @@ const NotFound: NextPage = () => {
             <p className="mt-2">The page you are looking for does not exist.</p>
             <button
               className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-              onClick={() => (window.location.href = "/")}
+              onClick={() => router.push("/")}
+              // onClick={() => (window.location.href = "/")}
             >
               Go Back Home
             </button>
@@ -34,5 +38,6 @@ const NotFound: NextPage = () => {
     </div>
   );
 };
+
 
 export default NotFound;
